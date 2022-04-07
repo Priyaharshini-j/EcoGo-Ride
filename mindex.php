d1df9dfbcea4a28352da56be8ad46adac814c2ca
Priyadharshini <jpd.priya2001@gmail.com>
2022-04-07 13:46:12 +0530

    Update mindex.php

--- mindex.php
+++ mindex.php
@@ -1,4 +1,4 @@
-﻿
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,24 +32,60 @@
       width: 80vw;
       align-items: center;
     }
+    *{
+    margin: 0;
+    padding: 0;
+}
+.logo
+{
+    width:90px;
+    cursor:pointer;
+    align-self: center;
+    background-color: white;
+}
+
+  #button-start, #button-stop , #myButton{
+   display: inline-flex;
+   height: 50px;
+   width: 120px;
+   border-style: solid;
+   justify-content: center;
+   padding: 15px;
+   line-height: 10px;
+   text-align:center;
+   background-color: rgb(168, 20, 20);
+   border: none;
+   outline: none;
+   border-radius: 10px;
+   overflow: hidden;
+    font-family: 'QuickSand',sans-serif;
+    font-size: 15px;
+    font-weight: 100;
+    cursor: pointer;
+    color: white;
+}
+#button-start:hover, #button-stop:hover , #myButton:hover{
+    background-color: rgba(222, 98, 104, 0.947);
+    color:black;
+    
+}
 
   </style>
   <script src="map.js" defer></script>
+
 </head>
 <body>
+        <div class="navbar">
+            <img src="images/logo.png" class="logo">
+            
+        </div>
   <center>
   <div id='map'></div>
   <div class="wrapper">
                 <p><span id="seconds">00</span>:<span id="tens">00</span></p><br/>
                 <button id="button-start" name="start" placeholder="Start Ride">START RIDE</button>
                 <button id="button-stop" name="stop" placeholder="End Ride" >END RIDE</button>
-                <button id="myButton" class="float-left submit-button" >Pay</button>
 
-<script type="text/javascript">
-    document.getElementById("myButton").onclick = function () {
-        location.href ="pay.php";
-    };
-</script>
                 <br/>
                 
                 <br/>
